6e1a1e5fe3e3ae16f03848cc407e389770473a52
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-16 20:41:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-studybot = Client("Full Study Bot",
+if __name__==__main__:
+  studybot= Client("Full Study Bot",
                   api_id=var.API_ID,
                  api_hash=var.API_HASH,
                  bot_token=var.BOT_API_TOKEN,
-                 plugins=dict(root="plugins"))
-
-studybot.run()
+                 plugins=plugins)
+  studybot.run()
