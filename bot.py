510e831dad9db9fc0e9aab46e8f4302d9efa5b88
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-16 20:48:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,13 @@ from Var import var
 import logging
 
 logging.basicConfig(level=logging.INFO)
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-if __name__==__main__:
+
+if __name__=="__main__":
   studybot= Client("Full Study Bot",
                   api_id=var.API_ID,
                  api_hash=var.API_HASH,
