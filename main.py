97d1ef570d7b1e2a6419482cba51f5ba34ce0227
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-17 01:03:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#================================================ # IMPORTS & BASIC SCHEME =========================================================
 import logging
 import asyncio as ae
 from telethon.sync import TelegramClient
@@ -7,11 +8,41 @@ from strings import strings
 from helpers import stored
 
 logging.basicConfig(level=logging.INFO)
-studybot = TelegramClient("Study Bot",api_id=var.API_ID,api_hash=var.API_HASH).start(bot_token=var.BOT_TOKEN)
+
+studybot = TelegramClient("Study Bot",
+                          api_id=var.API_ID,
+                          api_hash=var.API_HASH
+                         ).start(bot_token=var.BOT_TOKEN)
+
+#============================================================== # START  =========================================================
 
 @studybot.on(events.NewMessage(pattern="/start" or "/Start" or "/START"))
 async def startmessage(event):
-    await studybot.send_message(event.chat_id,strings.START_STRING,file=stored.START_PIC)
+    if event.is_private:
+        await studybot.send_message(event.chat_id,
+                                strings.START_STRING,
+                                file=stored.START_PIC)
+    else:
+        await studybot.send_message(event.chat_id,
+                                   "I Am Alive !")
+        
+        
+
+#======================================================== # BOT JOINED =========================================================
+
+@studybot.on(events.ChatAction)
+async def botadded(e):
+    bot = await studybot.get_me()
+    botid = bot.id
+    if e.is_added:
+        if e.user.id == botid:
+            await e.reply("Thanks for Adding Me !!")
+            
+    
+#============================================================== # END =========================================================
+    
+    
+    
     
     
 with studybot:
