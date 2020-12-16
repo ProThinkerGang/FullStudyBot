77ebe86016526cb7d61d274510176b05001136d5
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-17 00:16:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import asyncio as ae
 from telethon.sync import TelegramClient
 from telethon import events
 from Var import var
+from strings import START_STRING
 
 logging.basicConfig(level=logging.INFO)
 
 studybot = TelegramClient("Study Bot",api_id=var.API_ID,api_hash=var.API_HASH).start(bot_token=var.BOT_TOKEN)
 
-@studybot.on(events.NewMessage)
+@studybot.on(events.NewMessage(pattern="/start" or "/Start" or "/START"))
 async def startmessage(event):
-    await event.reply("Hi, I am Working")
+    await event.reply(START_STRING)
     
     
 with studybot:
