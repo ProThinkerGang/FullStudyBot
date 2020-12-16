ae368febc67b2f6bf671bcce2dfd42b67f4908c5
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-16 20:52:59 +0530

    Update start.py

--- plugins/basics/start.py
+++ plugins/basics/start.py
@@ -1,9 +1,10 @@
 from bot import studybot
 import logging
 from pyrogram import filters
+from pyrogram import Client
 
 logging.basicConfig(level=logging.INFO)
 
-@studybot.on_message(filters.private & filters.command("/start"))
+@Client.on_message(filters.private & filters.command("/start"))
 async def startmsg(client,message):
   await message.reply_text("Hi")
