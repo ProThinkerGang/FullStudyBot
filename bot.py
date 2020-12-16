c03d7a7f66bc259db5522e9bfc7b4c28caa0a3df
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-16 18:44:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
 from pyrogram import Client
+from Var import var
+
+studybot = Client(api_id=var.API_ID,
+                 api_hash=var.API_HASH,
+                 bot_token=var.BOT_API_TOKEN,
+                 plugins=dict(root="plugins"))
+
+studybot.run()
