0f1392982eb613a962855dd189ab2d8b47caed84
Anonymous <69723581+New-dev0@users.noreply.github.com>
2020-12-16 23:31:40 +0530

    Update Var.py

--- Var.py
+++ Var.py
@@ -5,4 +5,4 @@ load_dotenv(find_dotenv())
 class var:
   API_ID = os.getenv("API_ID", None) # API_ID
   API_HASH = os.getenv("API_HASH", None) #API_HASH
-  BOT_API_TOKEN = os.getenv("BOT_API_TOKEN", None) # Your Bot Api Token
+  BOT_TOKEN = os.getenv("BOT_TOKEN", None) # Your Bot Api Token
